80b40431ff8058d040b593cb5c52612c2f6d83d1
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2023-02-17 14:57:29 +0100

    Search bar to quickly access position

--- components/header.tsx
+++ components/header.tsx
@@ -1,28 +1,31 @@
 import React from "react";
 import Link from "next/link";
 import { MixerHorizontalIcon } from "@radix-ui/react-icons";
+import SearchBar from "@/components/search_bar";
 
 interface IProps {
   home: boolean;
 }
-const Header = ({ home }: IProps) => (
-  <header>
-    <div className="navbar bg-base-100 px-6">
-      <div className="flex-1">
-        {home ? (
-          <span className="normal-case text-xl">Mes postes ENM</span>
-        ) : (
-          <Link href="/" className="normal-case text-xl">
-            Mes postes ENM
-          </Link>
-        )}
+const Header = ({ home }: IProps) => {
+  return (
+    <header>
+      <div className="navbar bg-base-100 px-6">
+        <div className="flex-1">
+          {home ? (
+            <span className="normal-case text-xl">Mes postes ENM</span>
+          ) : (
+            <Link href="/" className="normal-case text-xl">
+              Mes postes ENM
+            </Link>
+          )}
+        </div>
+        {home ? <SearchBar /> : null}
+        <label htmlFor="drawer" className="drawer-button cursor-pointer">
+          <MixerHorizontalIcon className="h-6 w-6" />
+        </label>
       </div>
-
-      <label htmlFor="drawer" className="drawer-button cursor-pointer">
-        <MixerHorizontalIcon className="h-6 w-6" />
-      </label>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
---
+++ components/search_bar.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import Select, { ActionMeta, SingleValue } from "react-select";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { currentPositionIdAtom, positionsSelector } from "@/_state";
+import { Position } from "@/types/types";
+
+const SearchBar = () => {
+  const positions = useRecoilValue(positionsSelector);
+  const setCurrentPositionId = useSetRecoilState(currentPositionIdAtom);
+  const onSelect = (
+    position: SingleValue<Position>,
+    actionMeta: ActionMeta<Position>
+  ) => {
+    if (actionMeta.action == "select-option" && position) {
+      setCurrentPositionId(position.id);
+      // @ts-ignore
+      document.getElementById("modal").checked = true;
+    }
+  };
+
+  return (
+    <div className="form-control mr-20">
+      <div className="input-group input-group-lg">
+        <div className="border border-r-0 rounded-l-full p-1 flex items-center">
+          <MagnifyingGlassIcon className="h-8 w-8 text-base-300" />
+        </div>
+        <Select
+          className="input input-bordered p-0 w-96"
+          isSearchable={true}
+          isClearable={true}
+          placeholder="Chercher un poste..."
+          name="positions_search"
+          onChange={onSelect}
+          options={positions}
+          getOptionLabel={(option) =>
+            option.role?.name + " - " + option.tribunal?.name
+          }
+          getOptionValue={(option) => `${option.id}`}
+          styles={{
+            input: (baseStyles) => ({
+              ...baseStyles,
+              boxShadow: "none",
+            }),
+            control: (baseStyles) => ({
+              ...baseStyles,
+              border: 0,
+              height: "100%",
+              borderRadius: "0.5rem",
+            }),
+            menu: (baseStyles) => ({
+              ...baseStyles,
+            }),
+            indicatorSeparator: (baseStyles) => ({
+              ...baseStyles,
+              padding: 0,
+            }),
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
