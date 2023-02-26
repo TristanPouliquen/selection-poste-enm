15b5b2aa656a0cf02f439f4ba3efcbb436f17b15
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2023-02-26 19:23:51 +0100

    v0.1.1

--- src-tauri/src/models/mod.rs
+++ src-tauri/src/models/mod.rs
@@ -14,8 +14,7 @@ pub mod tribunal;
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    let database_url =
-        env::var("db/selection-poste-enm.sqlite3").expect("DATABASE_URL must be set");
+    let database_url = "db/selection-poste-enm.sqlite3";
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {database_url}"))
 }
