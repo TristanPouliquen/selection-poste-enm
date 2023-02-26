b1730ac049f9524e39ac123af1512e1300b610f9
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2023-02-26 20:04:41 +0100

    Lint Rust

--- src-tauri/src/models/mod.rs
+++ src-tauri/src/models/mod.rs
@@ -1,6 +1,4 @@
 use diesel::{Connection, SqliteConnection};
-use dotenvy::dotenv;
-use std::env;
 
 pub mod app_state;
 pub mod appeal_court;
@@ -12,9 +10,6 @@ pub mod time_window;
 pub mod tribunal;
 
 pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-
-    let database_url = "../../db/selection-poste-enm.sqlite3";
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {database_url}"))
+    SqliteConnection::establish("../../db/selection-poste-enm.sqlite3")
+        .unwrap_or_else(|_| panic!("Error connecting to database"))
 }
