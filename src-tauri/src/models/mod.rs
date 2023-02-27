f457d3f271df9a14b112cefaffbcde9d7b35ddb7
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2023-02-27 19:12:00 +0100

    Updater conf

--- src-tauri/src/models/mod.rs
+++ src-tauri/src/models/mod.rs
@@ -1,4 +1,5 @@
 use diesel::{Connection, SqliteConnection};
+use tauri::api::path::app_data_dir;
 
 pub mod app_state;
 pub mod appeal_court;
@@ -10,6 +11,8 @@ pub mod time_window;
 pub mod tribunal;
 
 pub fn establish_connection() -> SqliteConnection {
-    SqliteConnection::establish("../../db/selection-poste-enm.sqlite3")
+    let mut path = app_data_dir(&Default::default()).unwrap();
+    path.push("selection-poste-enm.sqlite3");
+    SqliteConnection::establish(&path.display().to_string())
         .unwrap_or_else(|_| panic!("Error connecting to database"))
 }
