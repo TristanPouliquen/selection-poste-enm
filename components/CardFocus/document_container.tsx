7a0beee10edb2fa9abcad9da0900fff9b0194ab2
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2023-03-01 17:34:58 +0100

    Awesome PDF preview

--- components/CardFocus/document_container.tsx
+++ components/CardFocus/document_container.tsx
@@ -1,10 +1,17 @@
-import React, { CSSProperties, MouseEvent, useEffect, useMemo } from "react";
+import React, {
+  CSSProperties,
+  MouseEvent,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { Document, Position } from "@/types/types";
 import {
   BaseDirectory,
   BinaryFileContents,
   createDir,
   exists,
+  readBinaryFile,
   removeFile,
   writeBinaryFile,
 } from "@tauri-apps/api/fs";
@@ -12,6 +19,12 @@ import { useDropzone } from "react-dropzone";
 import { useDocumentActions } from "@/_state/document";
 import { Cross1Icon, EyeOpenIcon, FileTextIcon } from "@radix-ui/react-icons";
 import prettyBytes from "pretty-bytes";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import { createPortal } from "react-dom";
+
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 interface IProps {
   position: Position;
@@ -47,9 +60,10 @@ const getFolderName = (position: Position) => `positions/${position.id}`;
 
 interface IDocumentProps {
   document: Document;
+  onTogglePreview: (document: Document) => void;
 }
 
-const DocumentIcon = ({ document }: IDocumentProps) => {
+const DocumentIcon = ({ document, onTogglePreview }: IDocumentProps) => {
   const { remove } = useDocumentActions();
 
   const onClick = async (e: MouseEvent) => {
@@ -59,7 +73,7 @@ const DocumentIcon = ({ document }: IDocumentProps) => {
   };
 
   return (
-    <div className="bordered rounded-lg bg-base-200/30 hover:bg-base-15 hover:text-base-content/25 p-2 pt-0 w-28 flex flex-col items-center text-center mb-4 relative">
+    <div className="bordered rounded-lg bg-base-200/30 hover:bg-base-15 hover:text-base-content/25 p-2 pt-0 w-28 mr-6 flex flex-col items-center text-center mb-4 relative">
       <div
         className="badge badge-outline z-20 px-1 border-base-200 bg-base-200/50 hover:bg-base-200 hover:cursor-pointer absolute -top-2 -right-2"
         onClick={onClick}
@@ -70,16 +84,54 @@ const DocumentIcon = ({ document }: IDocumentProps) => {
       <span className="text-xs">
         {`${document.name} (${prettyBytes(document.size, { locale: "fr" })})`}
       </span>
-      <div className="absolute rounded-lg z-10 w-full h-full flex items-center justify-center bg-neutral-content/50 opacity-0 hover:opacity-100">
+      <div className="absolute rounded-lg z-10 w-full h-full flex items-center justify-center bg-neutral-content/30 opacity-0 hover:opacity-100">
         <EyeOpenIcon
           className="h-8 w-8 opacity-100 text-base-content cursor-pointer"
-          onClick={console.log}
+          onClick={() => onTogglePreview(document)}
         />
       </div>
     </div>
   );
 };
+
+const DocumentModal = ({ document, onTogglePreview }: IDocumentProps) => {
+  const [fileContents, setFileContents] = useState<Uint8Array>();
+  useEffect(() => {
+    readBinaryFile(document.path, { dir: BaseDirectory.AppData }).then(
+      (content) => {
+        setFileContents(content);
+      }
+    );
+  }, [readBinaryFile, setFileContents]);
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  return (
+    <div className="bg-base-100 flex flex-col overflow-hidden fixed top-0 left-0 h-full w-full fixed z-100">
+      <div className="flex items-center bg-base-300/50 p-2">
+        <div className="mr-auto">{document.name}</div>
+        <button
+          className="btn btn-sm btn-primary rounded cursor-pointer"
+          onClick={() => onTogglePreview(document)}
+        >
+          Fermer
+        </button>
+      </div>
+      <div className="grow overflow-auto">
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.js">
+          {fileContents && (
+            <Viewer
+              fileUrl={fileContents}
+              plugins={[defaultLayoutPluginInstance]}
+            />
+          )}
+        </Worker>
+      </div>
+    </div>
+  );
+};
 const DocumentContainer = ({ position }: IProps) => {
+  const [previewed, setPreviewed] = useState<Document | undefined>(undefined);
+  const onTogglePreview = (document: Document) =>
+    setPreviewed(previewed ? undefined : document);
   const { getAll, create } = useDocumentActions();
   useEffect(() => {
     if (position.documents === undefined) {
@@ -156,6 +208,7 @@ const DocumentContainer = ({ position }: IProps) => {
         {(position.documents ?? []).map((document) => (
           <DocumentIcon
             document={document}
+            onTogglePreview={onTogglePreview}
             key={`document_icon_${document.id}`}
           />
         ))}
@@ -165,6 +218,15 @@ const DocumentContainer = ({ position }: IProps) => {
         Cliquez ou glissez-déposez de nouveaux PDF ici pour les ajouter à la
         fiche.
       </div>
+      {previewed &&
+        createPortal(
+          <DocumentModal
+            key={`document_modal_${previewed.id}`}
+            document={previewed}
+            onTogglePreview={onTogglePreview}
+          />,
+          document.body
+        )}
     </div>
   );
 };
